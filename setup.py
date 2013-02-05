61b648847098bcbf9429e4cbf6f00bdbb50b8869
ymotongpoo <ymotongpoo@gmail.com>
2013-02-05 23:28:54 +0900

    fixed version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def _read(name):
     except:
         return ""
 
-version = '0.2.2'
+version = '0.2.3'
 name = 'suddendeath'
 short_description = '`suddendeath` generates "突然の死" message.'
 readme = _read('README')
