23e9a825e98945b31c4a95d58437bede182c2203
Yoshi Yamaguchi <ymotongpoo@gmail.com>
2017-05-11 11:19:03 -0700

    created wheel

--- setup.py
+++ setup.py
@@ -26,12 +26,10 @@ CLASSIFIERS = [
     'Development Status :: 2 - Pre-Alpha',
     'License :: OSI Approved :: Apache Software License',
     'Programming Language :: Python',
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.6',
-    'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
     'Topic :: Utilities',
 ]
 
